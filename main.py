f3d712c2c81f1b7fae409acc43cb21e75f3524b6
Devchonka <menyaylenko@gmail.com>
2015-03-19 16:54:54 -0700

    adding specs

--- main.py
+++ main.py
@@ -32,9 +32,7 @@ import qualification
 
 def read_data_file(filename):
     """
-    Function that reads input file and returns the data in form of a dataframe.
-    :param filename: name of data input file as .txt
-    :return: dataframe object of the data contained in input file
+    Function that reads input file and returns the accel reading data in form of a dataframe.
     """
     df = read_csv(filename, sep='\t', lineterminator='\n', header=[0, 1], index_col=0)
     return df
@@ -44,23 +42,17 @@ def calc_stats(df):
     # The y values.  A numpy array is used here,
     # but a python list could also be used.
     dx = df.index[1] - df.index[0]
-    Grms_total, Grms_peak = get_Grms(df.ix[:, 0].tolist(), dx)
-
+    Grms_total = get_Grms(df.ix[:, 0].tolist(), dx)
     print('Grms_total = ', Grms_total)
-    print('Grms_peak = ', Grms_peak)
+
 
 
 def get_Grms(list, dx):
     """
     Function to numerically compute area under ASD curve (Grms value) using composite Simpson's rule.
-    :param list: list of values of ASD vector
-    :return: Grms values of the whole curve, and of the curve up until the max peak
     """
-    peak_index = list.index(max(list))
-    # peak_index = list.idxmax()
     Grms_total = simps(list, dx=dx)
-    Grms_peak = simps(list[0:peak_index], dx=dx)
-    return Grms_total, Grms_peak
+    return Grms_total
 
 
 def main():
@@ -69,14 +61,15 @@ def main():
     fname_design_loads = 'design_loads.txt'
     fname_randomV_specs = 'qual_specs.txt'
     data_df = read_data_file(fname_data)  # create data object that will keep all variables from file, raw and processed
-    spec_df = qualification.get_qual(fname_randomV_specs)
+    spec_dict = qualification.get_qual(fname_randomV_specs)
+
 
 
     # get dictionary of design loads of {figure# : design_load}
     dict_loads = qualification.get_design_loads(fname_design_loads)
 
     calc_stats(data_df)
-    plotting.make_plots(data_df, spec_df)
+    plotting.make_plots(data_df, spec_dict)
 
 if __name__ == '__main__':
     main()
--- qualification.py
+++ qualification.py
@@ -29,24 +29,62 @@ def get_qual_specs(fname):
 #     slopes = [6.0, 0.08, -4.5]
 #     return breakpoints, slopes
 
+
+def build_continuous(breakpoints,slopes, freq):
+    """
+    Function to take a vector of breakpoints, and one of slopes and to build continuos spec lines
+    Input is based on number of breakpoints in spec
+    """
+
+    if len(breakpoints) == 2:
+        conditions = [freq < breakpoints[0],
+                      (freq >= breakpoints[0]) & (freq < breakpoints[1]),
+                      freq >= breakpoints[1]]
+
+        functions = [lambda freq: slopes[1] - slopes[0] * (breakpoints[0] - freq),
+                     lambda freq: slopes[1],
+                     lambda freq: slopes[1] + slopes[2] * (freq - breakpoints[1])]
+    elif len(breakpoints) == 4:
+        conditions = [freq < breakpoints[0],
+                      (freq >= breakpoints[0]) & (freq < breakpoints[1]),
+                      (freq >= breakpoints[1]) & (freq < breakpoints[2]),
+                      (freq >= breakpoints[2]) & (freq < breakpoints[3]),
+                      freq >= breakpoints[3]]
+
+        functions = [lambda freq: slopes[1] - slopes[0] * (breakpoints[0] - freq),
+                     lambda freq: slopes[1],
+                     lambda freq: slopes[3] - slopes[2] * (breakpoints[2] - freq),
+                     lambda freq: slopes[3],
+                     lambda freq: slopes[3] + slopes[4] * (freq - breakpoints[3])]
+    else:
+        raise IOError
+    return piecewise(freq, conditions, functions)
+
+
+
 def get_qual(fname):
     """
     Breakpoints denoted b0,b1...slopes or flat values denoted s0,s1,s2
     """
     qual_df = get_qual_specs(fname)
-    breakpoints,slopes = qual_df['fig1_b'], qual_df['fig1_s']
-
+    keys = ['fig1','fig2','fig3','fig4','fig5','fig6','fig7','fig8_IP','fig8_OP',
+            'fig9','fig10_IP','fig10_OP','fig11','fig12','fig13']
+    dict_specs = {}
     dx = 4.0  # from data collected
     # freq = list(frange(20.0, 2504.0, dx))
     # k = array([x for x in frange(20.0, 2500.6, dx)])
     freq = arange(20.0, 2504.0, dx)
-    conditions = [freq < breakpoints[0],
-                  (freq >= breakpoints[0]) & (freq < breakpoints[1]),
-                  freq >= breakpoints[1]]
+    keys_iter = 0
 
-    functions = [lambda freq: slopes[1] - slopes[0] * (breakpoints[0] - freq),
-                 lambda freq: slopes[1],
-                 lambda freq: slopes[1] + slopes[2] * (freq - breakpoints[1])]
+    for iter in range(len(qual_df.columns))[0::2]:
 
-    return piecewise(freq, conditions, functions)
+        breakpoints, slopes = qual_df.ix[:, iter].dropna(),qual_df.ix[:, iter+1].dropna()
+
+        # append piecewise to dict_specs
+        dict_specs[keys[keys_iter]] = build_continuous(breakpoints,slopes, freq)
+        keys_iter += 1
+
+
+
+    return dict_specs
 
