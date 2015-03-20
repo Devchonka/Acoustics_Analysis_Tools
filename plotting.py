a69beab2bf6adbe55b5c37712d354c412f5f1460
Devchonka <menyaylenko@gmail.com>
2015-03-20 15:40:29 -0700

    plots

--- plotting.py
+++ plotting.py
@@ -159,5 +159,54 @@ def make_plots(data_df, spec_dict):
 
     plt.savefig('fig9.pdf',bbox_inches='tight')
 
+    # Figure (10) : TC & R
+    ax = data_df.plot(y=[36,37,38], title='Accelerometer Overtests', loglog=True)
+    plt.plot(data_df.index, spec_dict['fig10'], color='black', linewidth=2.5, linestyle='--')
+    plt.xlim([10, 2 ** 12])
+    fig = ax.get_figure()
+
+    fig.suptitle('NBN co 1-A : TC & R', fontsize=20)
+    plt.xlabel('Hz', fontsize=18)
+    plt.ylabel('Acceleration Spectral Density (ASD) ($g^2/Hz$)', fontsize=16)
+    plt.legend(loc=0, prop={'size':6})
+    plt.savefig('fig10.pdf',bbox_inches='tight')
+
+
+    # Figure (11) : LNA Panels
+
+    plt.figure()
+    plt.suptitle('NBN co 1-A : LNA Panels', fontsize=20)
+    ax = plt.subplot("121")
+    ax.set_title('In Plane')
+
+    ax.loglog (data_df.index, data_df[[42]], label=data_df.columns[42])
+    ax.loglog (data_df.index, data_df[[44]], label=data_df.columns[44])
+    ax.loglog (data_df.index, data_df[[46]], label=data_df.columns[46])
+
+    plt.loglog(data_df.index, spec_dict['fig11_IP'], color='black', linewidth=2.5, linestyle='--')
+    plt.xlim([10, 2 ** 12])
+    plt.xlabel('Hz', fontsize=11)
+    plt.ylabel('Acceleration Spectral Density (ASD) ($g^2/Hz$)', fontsize=10)
+    plt.grid()
+    plt.legend(loc=0, prop={'size':6})
+
+    ax = plt.subplot("122")
+    ax.set_title('Out of Plane')
+
+    ax.loglog (data_df.index, data_df[[39]], label=data_df.columns[39])
+    ax.loglog (data_df.index, data_df[[40]], label=data_df.columns[40])
+    # ax.loglog (data_df.index, data_df[[41]], label=data_df.columns[41])
+    ax.loglog (data_df.index, data_df[[43]], label=data_df.columns[43])
+    ax.loglog (data_df.index, data_df[[45]], label=data_df.columns[45])
+    ax.loglog (data_df.index, data_df[[47]], label=data_df.columns[47])
+
+    plt.loglog(data_df.index, spec_dict['fig11_OP'], color='black', linewidth=2.5, linestyle='--')
+    plt.xlim([10, 2 ** 12])
+    plt.xlabel('Hz', fontsize=11)
+    plt.grid()
+    plt.legend(loc=0, prop={'size':6})
+
+    plt.savefig('fig11.pdf',bbox_inches='tight')
+
 
     #plt.show()
